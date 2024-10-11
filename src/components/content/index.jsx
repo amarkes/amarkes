c05995540b876daa92d1774b223f6130d92e26a4
Antonio Marques <tmowna@gmail.com>
2024-10-11 16:51:13 -0300

    fix: responsive

--- src/components/content/index.jsx
+++ src/components/content/index.jsx
@@ -5,7 +5,6 @@ import HeaderComponent from '@/components/header/index';
 import FooterComponent from '@/components/footer/index';
 import { ThemeContext } from '@/contexts/theme/themeContext';
 
-
 import ExperiencePage from '@/pages/experience';
 import TrainingPage from '@/pages/training';
 import SkillsPage from '@/pages/skills';
@@ -23,6 +22,7 @@ const tabContent = {
 const ContentComponent = () => {
   const { theme } = useContext(ThemeContext);
   const [activeTab, setActiveTab] = useState('perfil');
+  const [isMobile, setIsMobile] = useState(false);
   const contentRef = useRef(null);
 
   const openTab = (tabId) => {
@@ -30,15 +30,45 @@ const ContentComponent = () => {
   };
 
   useEffect(() => {
+    // Detectar se é um dispositivo móvel com base na largura da tela
+    const checkIfMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    // Checar inicialmente
+    checkIfMobile();
+
+    // Adicionar evento de redimensionamento para atualizar conforme necessário
+    window.addEventListener('resize', checkIfMobile);
+
+    // Limpar o evento ao desmontar
+    return () => window.removeEventListener('resize', checkIfMobile);
+  }, []);
+
+  useEffect(() => {
     if (contentRef.current) {
       contentRef.current.scrollTop = 0;
     }
   }, [activeTab]);
 
+  useEffect(() => {
+    // Desabilitar o scroll do body se estiver em um dispositivo móvel
+    if (isMobile) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+
+    // Restaurar o scroll do body ao desmontar
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [isMobile]);
+
   return (
     <div className={`flex justify-center items-center h-screen ${theme === 'dark' ? 'bg-gray-900' : 'bg-gray-100'} font-sans`}>
-      <div className={`w-[390px] h-[90vh] rounded-2xl ${theme === 'dark' ? 'bg-gray-500' : 'bg-black'} p-1 shadow-md flex justify-center items-center`}>
-        <div className={`w-[380px] h-[87vh] ${theme === 'dark' ? 'bg-gray-900' : 'bg-white'} rounded-lg shadow-md overflow-hidden text-center relative flex flex-col`}>
+      <div className={`${isMobile ? 'h-[100vh]' : 'w-[390px] h-[85vh] rounded-2xl'} ${theme === 'dark' ? 'bg-gray-500' : 'bg-black'} p-1 shadow-md flex justify-center items-center`}>
+        <div className={`w-full ${isMobile ? 'h-full' : 'w-[385px] h-[80vh]'} ${theme === 'dark' ? 'bg-gray-900' : 'bg-white'} ${isMobile ? '' : 'rounded-lg'} shadow-md overflow-hidden text-center relative flex flex-col`}>
           <HeaderComponent />
           <div ref={contentRef} className="flex-1 overflow-y-auto">
             {tabContent[activeTab]}
